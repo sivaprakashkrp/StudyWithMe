5addaa685b1e191499dc8a713240b2a2e90f0def
A P. RAJ JAGADEESH <rajjagadeesh2006@gmail.com>
2026-02-02 11:24:33 +0530

    Refined UI for User Greetings, Start the new meeting in the Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Dashboard = () => {
     const handleStartMeeting = () => {
         console.log('Starting a new meeting...');
     };
     return (
-        <main className="flex-1 flex flex-col items-center pt-16 px-8 bg-white">
+        <>
+            <Navbar />
+            <main className="flex-1 flex flex-col pt-8 px-8 bg-white">
         <div className="w-full max-w-4xl">
             {/* User Greeting */}
-            <h1 className="text-3xl font-medium mb-16 text-left">
+            <h1 className="font-bold font-days text-5xl mb-16 text-left">
                 Hello, User!
             </h1>
         </div>
 
-        <div className="flex flex-col items-center">
-            <h3 className="text-2xl mb-8 font-light">Start a new meeting</h3>
+        <div className="flex flex-col items-center pb-16">
+            <h3 className="text-4xl mb-8 font-days">Start a new meeting</h3>
 
             {/* Dashboard Action Button */}
             <button
                 onClick={handleStartMeeting}
-                className="flex flex-col bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700 transition"
+                className="flex flex-col bg-gray-300 items-center-safe text-black px-26 py-18 rounded-md hover:bg-gray-400 transition"
             >
-                <div className="w-20 h-20 border-2 border-black rounded-full flex items-center justify-center mb-6 group-hover:bg-black group-hover:text-white transition-colors">
+                <div className="w-25 h-25 border-2 bg-swm-blue border-black rounded-full flex items-center justify-center mb-6 group-hover:bg-black group-hover:text-white transition-colors">
                     <span className="text-5xl font-light leading-none">+</span>
                 </div>
 
-                <span className="text-xl font-medium tracking-wide">
+                <span className="text-xl font-days tracking-wide">
                     Start A New Instant Meeting
                 </span>
             </button>
         </div>
     </main>
+        </>
     );
 };
 
