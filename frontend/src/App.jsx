e29949bc173c904741d3132f6b5d408df552d369
TARUN-B-S <tarunbs2007@gmail.com>
2026-02-09 10:38:35 +0530

    added SignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path='/Signup' element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import '../css/signup.css'
 
 const Signup = () => {
   return (
     <React.Fragment>
-        <Navbar />
+      <Navbar />
+      <MainBody />
     </React.Fragment>
   )
 }
 
-export default Signup
+const MainBody = () => {
+  return (
+    <>
+      <div class="main">
+        <div class="main-left">
+          <img src="your-image.png" alt="study"/>
+        </div>
+
+        <div class="main-right">
+
+          <div class="form-card">
+            <label>Email</label>
+            <input type="email"/>
+
+              <label>Username</label>
+              <input type="text"/>
+
+                <label>Password</label>
+                <input type="password"/>
+
+                  <button>Sign Up</button>
+          </div>
+
+                <p class="login-text">
+                  Have an Account already?  <span>Sign In</span>
+                </p>
+        </div>
+      </div>
+        </>)
+}
+
+        export default Signup
